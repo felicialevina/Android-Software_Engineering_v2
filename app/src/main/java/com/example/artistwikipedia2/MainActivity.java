2e5a537402943203e33f0bba2fb1929ce330cb8b
Felicia Levina <felicialevina10@gmail.com>
2020-10-08 16:40:50 -0400

    Splash Activity Completed

--- app/src/main/java/com/example/artistwikipedia2/MainActivity.java
+++ app/src/main/java/com/example/artistwikipedia2/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.artistwikipedia2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audio = new MediaPlayer();
+        audio = MediaPlayer.create(this, R.raw.dynamite);
+        audio.start();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(MainActivity.this, ArtistListActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task, 5000);
+
+
     }
 }
